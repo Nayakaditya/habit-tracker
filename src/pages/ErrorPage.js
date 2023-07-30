a091db22448cdfe66169d9340953dec3aa192876
Sachin Nyk <140935272+itsAdi007@users.noreply.github.com>
2023-07-30 11:03:20 +0530

    Update ErrorPage.js
    Add some extra line of code for error message

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function errorPage() {
-  return <div>errorPage</div>;
+  return (
+      <div className="grid place-items-center w-full mt-20 text-gray-400">
+        <h1><span className="text-white" >404</span> page not found!</h1>
+        <p>Looks like you've been forgotten your way</p>
+      </div>
+    )
 }
